19e46a8f72dab261a6a8fde53b960af8e03cb278
khatriamit <rajukhatri054@gmail.com>
2022-05-07 10:14:14 +0545

    add docs for macro

--- src/bin/p2.rs
+++ src/bin/p2.rs
@@ -1,3 +1,15 @@
+// Topic: Try test macro basic implementation
+//
+// fragment specifiers =  expr, ident, path, ty, item, tt, block and so on
+// description = expression, identifier, path, type, top level item, token-tree, block and so on
+// Examples:
+// expr = 3,x, func(a+2), if c{1} else {2}, loop {}
+// ident = x, main, my_test_macro
+// path = x, main, module::Type, ::crate::Type, x::<T>::y, <T>::foo
+// item = extern "C", fn foo(), use std::fs;, struct Test;
+// tt= fn, main, (), (3)
+// {},{3},{let x = 4}
+
 macro_rules! my_test_macro {
     ($val: expr) => {
         let x = $val;
